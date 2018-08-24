63be7e6c985c5375ba74114dffce2f7c97a3cd4f
joymon <joymon@gmail.com>
2018-08-24 12:23:09 -0400

    Initial check in

---
+++ DotNet.Helpers/Core/ActionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet.Helpers.Core
+{
+    public static class ActionExtensions
+    {
+        public static void ExecuteWithCatchAndFinally(this Action action, Action<Exception> catchBody, Action finalBody)
+        {
+            try
+            {
+                action();
+            }
+            catch (Exception ex)
+            {
+                catchBody(ex);
+            }
+            finally
+            {
+                finalBody();
+            }
+
+        }
+    }
+}
---
+++ DotNet.Helpers/Linq/EnumerableExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DotNet.Helpers.Linq
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Chunk the sequence based on chunkSize and returns sequence of chunks.
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="chunkSize"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<TResult>> Chunk<TResult>(this IEnumerable<TResult> source, int chunkSize)
+        {
+            return source.
+                Select((thread, index) => new { Index = index, Value = thread }).
+                GroupBy(tuple => tuple.Index / chunkSize).
+                Select(group => group.Select(tuple => tuple.Value));
+        }
+        /// <summary>
+        /// Randomize the sequence
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<TResult> Randomize<TResult>(this IEnumerable<TResult> source)
+        {
+            return source.
+                Select((sourceItem, index) => new
+                {
+                    Item = sourceItem,
+                    Id = Guid.NewGuid()
+                }).
+            OrderBy(t1 => t1.Id).Select(t1 => t1.Item);
+        }
+    }
+}
