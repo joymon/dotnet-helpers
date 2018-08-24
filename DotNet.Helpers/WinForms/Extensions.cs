8a6a162cc0df50fcc61eb309a0bf3b879a57a92f
joymon <joymon@gmail.com>
2018-08-24 14:46:40 -0400

    Added support for WinForms

---
+++ DotNet.Helpers/WinForms/Extensions.cs
@@ -0,0 +1,39 @@
+﻿#if NETFULL
+using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DotNet.Helpers.Core;
+namespace DotNet.Helpers.WinForms
+{
+    public static class Extensions
+    {
+        public static void ExecutePreTryCatchWithMessageAndFinally(Action preAction, Action action, Action final)
+        {
+            preAction();
+            action.ExecuteWithCatchAndFinally((ex => MessageBox.Show($"Exception - {ex}")), final);
+        }
+        public static async Task ExecuteWithEnableAndDisableAsync(this Control btn, Action action)
+        {
+            WinForms.Extensions.ExecutePreTryCatchWithMessageAndFinally(
+                () => btn.Enabled = false,
+                () => action(),
+                () => btn.Enabled = true);
+        }
+        public static void InvokeIfRequired(this ISynchronizeInvoke _target, Action action)
+        {
+            if (_target.InvokeRequired)
+            {
+                _target.BeginInvoke(new Action(() =>
+                {
+                    action();
+                }), null);
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
+#endif
---
+++ DotNet.Helpers/WinForms/TextBoxTraceListener.cs
@@ -0,0 +1,37 @@
+﻿#if NETFULL
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+using System.Windows.Forms;
+
+namespace DotNet.Helpers.WinForms
+{
+    public class TextBoxTraceListener : TraceListener
+    {
+        private TextBox _target;
+
+        public TextBoxTraceListener(TextBox target)
+        {
+            _target = target;
+        }
+
+        public override void Write(string message)
+        {
+            lock (lockObj)
+            {
+                _target.InvokeIfRequired(() => _target.AppendText(message));
+            }
+        }
+        object lockObj = new object();
+        public override void WriteLine(string message)
+        {
+            lock (lockObj)
+            {
+                _target.InvokeIfRequired(() => _target.AppendText(message + Environment.NewLine));
+            }
+        }
+    }
+}
+#endif
