ebd9eac413fe4b56cc2c696f0fd7a8fafea9f830
joymon <joymon@gmail.com>
2018-08-25 11:02:07 -0400

    Added test cases

--- DotNet.Helpers.Tests/Linq/EnumerableExtensions_Chunk.cs
+++ DotNet.Helpers.Tests/Linq/EnumerableExtensions_Chunk.cs
@@ -9,7 +9,7 @@ namespace DotNet.Helpers.Tests.Linq
     public class EnumerableExtensions_Chunk
     {
         [TestMethod]
-        public void Test1()
+        public void WhenInputHas4ElementsAndChunkSizeIs2_Return2Chunks()
         {
             IEnumerable<int> input = new List<int>() { 1, 2, 3, 4 };
             int expected1 = 3;
@@ -17,6 +17,19 @@ namespace DotNet.Helpers.Tests.Linq
 
             int actual1 = input.Chunk(2).First().Sum();
             int actual2 = input.Chunk(2).Last().Sum();
+            bool result = expected1 == actual1 && expected2 == actual2;
+
+            Assert.AreEqual(result, true);
+        }
+        [TestMethod]
+        public void WhenInputHas4ElementsAndChunkSizeIs3_Return2Chunks()
+        {
+            IEnumerable<int> input = new List<int>() { 1, 2, 3, 4 };
+            int expected1 = 6;
+            int expected2 = 4;
+
+            int actual1 = input.Chunk(3).First().Sum();
+            int actual2 = input.Chunk(3).Last().Sum();
             bool result = expected1 == actual1 && expected2 == actual2;
 
             Assert.AreEqual(result, true);
