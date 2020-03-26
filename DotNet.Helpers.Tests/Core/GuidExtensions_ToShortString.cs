b28c4a1bb1db632db182c63b24336936a224d95c
Joy George Kunjikkuru <joymon@gmail.com>
2020-03-26 10:30:10 -0400

    Added Guid.ToShortString() extension method for shorting GUIDs

---
+++ DotNet.Helpers.Tests/Core/GuidExtensions_ToShortString.cs
@@ -0,0 +1,18 @@
+﻿using DotNet.Helpers.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DotNet.Helpers.Tests.Core
+{
+    [TestClass]
+    public class GuidExtensions_ToShortString
+    {
+        [TestMethod]
+        public void WhenGuidToShortStringIsCalled_ReturnShortId()
+        {
+            Guid id = Guid.NewGuid();
+            string shortId= id.ToShortString();
+            Assert.IsNotNull(shortId, $"Received value {shortId}is null");
+        }
+    }
+}
---
+++ DotNet.Helpers/Core/GuidExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DotNet.Helpers.Core
+{
+    /// <summary>
+    /// Extensions on <see cref="Guid"/> class.
+    /// </summary>
+    public static class GuidExtensions
+    {
+        /// <summary>
+        /// Converts to shortstring having 22 characters
+        /// </summary>
+        /// <param name="id">The Guid to convert.</param>
+        /// <returns>Short <see cref="string"/> id.</returns>
+        public static string ToShortString(this Guid id)
+        {
+            return string.Empty;
+        }
+    }
+}
